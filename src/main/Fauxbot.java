ae1e6cb84fcc8791ffb4e84cee0df5d9fa7f763f
WRall <wm_rall@hotmail.com>
2016-11-12 16:59:41 -0800

    Remove dead code...

--- src/main/Fauxbot.java
+++ src/main/Fauxbot.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
