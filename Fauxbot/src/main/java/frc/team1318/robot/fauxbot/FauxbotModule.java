0a1413b5833061d83f88f04618bc25920e834a84
William Rall <William Rall>
2018-07-23 01:01:37 -0700

    Remove reference implementations

--- Fauxbot/src/main/java/frc/team1318/robot/fauxbot/FauxbotModule.java
+++ Fauxbot/src/main/java/frc/team1318/robot/fauxbot/FauxbotModule.java
@@ -1,8 +1,6 @@
 package frc.team1318.robot.fauxbot;
 
-import frc.team1318.robot.fauxbot.simulation.DriveTrainSimulator;
 import frc.team1318.robot.fauxbot.simulation.ElevatorSimulator;
-import frc.team1318.robot.fauxbot.simulation.GarageDoorSimulator;
 
 import com.google.inject.AbstractModule;
 
@@ -11,8 +9,6 @@ public class FauxbotModule extends AbstractModule
     @Override
     protected void configure()
     {
-        this.bind(IRealWorldSimulator.class).to(GarageDoorSimulator.class);
-        //this.bind(IRealWorldSimulator.class).to(ElevatorSimulator.class);
-        //this.bind(IRealWorldSimulator.class).to(DriveTrainSimulator.class);
+        this.bind(IRealWorldSimulator.class).to(ElevatorSimulator.class);
     }
 }
