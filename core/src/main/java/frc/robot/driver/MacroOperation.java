812efb7f4ed7dfeca1fa51dc7cbf630bea94d712
William Rall <William Rall>
2019-05-15 01:01:02 -0700

    Remove unused macro operations

--- core/src/main/java/frc/robot/driver/MacroOperation.java
+++ core/src/main/java/frc/robot/driver/MacroOperation.java
@@ -2,15 +2,5 @@ package frc.robot.driver;
 
 public enum MacroOperation
 {
-    // DriveTrain operations:
     AutonomousRoutine,
-    PIDBrake,
-    TurnInPlaceLeft,
-    TurnInPlaceRight,
-    FollowSomePath,
-
-    // Vision operations:
-    VisionCenterAndAdvance,
-    VisionFastCenterAndAdvance,
-    DriveForwardTurnRight,
 }
