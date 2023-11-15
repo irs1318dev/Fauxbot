b4a4b5810f558fcb106ac97ce52b3ac64483b37c
William Rall <wm_rall@hotmail.com>
2023-11-15 01:40:39 -0800

    Preliminary ForkliftSimulator UI

--- fauxbot_core/src/main/java/frc/robot/simulation/ForkliftSimulator.java
+++ fauxbot_core/src/main/java/frc/robot/simulation/ForkliftSimulator.java
@@ -1,28 +1,26 @@
 package frc.robot.simulation;
 
-import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 import frc.lib.robotprovider.*;
-import frc.robot.HardwareConstants;
-import frc.robot.TuningConstants;
+import space.earlygrey.shapedrawer.ShapeDrawer;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-
-// import javafx.scene.canvas.Canvas;
-// import javafx.scene.canvas.GraphicsContext;
-// import javafx.scene.image.Image;
-// import javafx.scene.paint.Color;
-// import javafx.scene.transform.Affine;
-// import javafx.scene.transform.Rotate;
 
 @Singleton
 public class ForkliftSimulator extends SimulatorBase
 {
-    private static final double WHEEL_SEPARATION_DISTANCE = 10.0; // in inches/pixels
-    private static final double FORKLIFT_SPEED = 10.0; // in inches/pixels / sec
+    private static final float WHEEL_SEPARATION_DISTANCE = 10.0f; // in inches/pixels
+    private static final float FORKLIFT_SPEED = 10.0f; // in inches/pixels / sec
 
     private static final FauxbotActuatorConnection LeftMotorConnection = new FauxbotActuatorConnection(FauxbotActuatorConnection.ActuatorConnector.PWM, 0);
     private static final FauxbotActuatorConnection RightMotorConnection = new FauxbotActuatorConnection(FauxbotActuatorConnection.ActuatorConnector.PWM, 1);
@@ -54,61 +52,62 @@ public class ForkliftSimulator extends SimulatorBase
         }
     };
 
-    private static final double FORKLIFT_LENGTH = 20.0; // front to back, in pixels
-    private static final double FORKLIFT_WIDTH = 10.0; // left to right, in pixels
-    private static final double FORKLIFT_HALF_LENGTH = ForkliftSimulator.FORKLIFT_LENGTH / 2.0;
-    private static final double FORKLIFT_HALF_WIDTH = ForkliftSimulator.FORKLIFT_WIDTH / 2.0;
+    private static final float FrameDimension = 400.0f;
 
-    private static final double MAX_X = 125.0;
-    private static final double MAX_Y = 200.0;
-    private static final double STARTING_ANGLE_R = Math.PI / 2.0; // 90deg to the left (image-up)
-    private static final double STARTING_X = ForkliftSimulator.MAX_X / 2.0;
-    private static final double STARTING_Y = ForkliftSimulator.MAX_Y / 2.0;
-    private static final double MAX_WALL_DISTANCE =
-        Math.sqrt(FORKLIFT_HALF_LENGTH * FORKLIFT_HALF_LENGTH + FORKLIFT_HALF_WIDTH * FORKLIFT_HALF_WIDTH);
+    private static final float FORKLIFT_LENGTH = 20.0f; // front to back, in pixels
+    private static final float FORKLIFT_WIDTH = 10.0f; // left to right, in pixels
+    private static final float FORKLIFT_HALF_LENGTH = ForkliftSimulator.FORKLIFT_LENGTH / 2.0f;
+    private static final float FORKLIFT_HALF_WIDTH = ForkliftSimulator.FORKLIFT_WIDTH / 2.0f;
 
-    private double leftPower;
-    private double rightPower;
+    private static final float MAX_X = 250.0f;
+    private static final float MAX_Y = 400.0f;
+    private static final float STARTING_ANGLE_R = (float)Math.PI / 2.0f; // 90deg to the left (image-up)
+    private static final float STARTING_X = ForkliftSimulator.MAX_X / 2.0f;
+    private static final float STARTING_Y = ForkliftSimulator.MAX_Y / 2.0f;
+    private static final float MAX_WALL_DISTANCE =
+        (float)Math.sqrt(FORKLIFT_HALF_LENGTH * FORKLIFT_HALF_LENGTH + FORKLIFT_HALF_WIDTH * FORKLIFT_HALF_WIDTH);
+
+    private float leftPower;
+    private float rightPower;
 
     private boolean forkliftUp;
 
-    // private Image forkliftDownImage;
-    // private Image forkliftUpImage;
+    private Texture forkliftDownImage;
+    private Texture forkliftUpImage;
+    private Texture drawerTexture;
 
     // odometry coordinates (x forward, y left, angle counter-clockwise)
-    private double x;
-    private double y;
-    private double angle;
-    private double prevLeftDistance;
-    private double prevRightDistance;
+    private float x;
+    private float y;
+    private float angle;
+    private float prevLeftDistance;
+    private float prevRightDistance;
 
     @Inject
     public ForkliftSimulator()
     {
-        this.leftPower = 0.0;
-        this.rightPower = 0.0;
+        this.leftPower = 0.0f;
+        this.rightPower = 0.0f;
 
         // start with it either up or down
         this.forkliftUp = Math.random() >= 0.5;
 
         this.x = ForkliftSimulator.STARTING_X;
         this.y = ForkliftSimulator.STARTING_Y;
-        this.angle = ForkliftSimulator.STARTING_ANGLE_R * (180.0 / Math.PI);
-        this.prevLeftDistance = 0.0;
-        this.prevRightDistance = 0.0;
+        this.angle = ForkliftSimulator.STARTING_ANGLE_R * (180.0f / (float)Math.PI);
+        this.prevLeftDistance = 0.0f;
+        this.prevRightDistance = 0.0f;
 
-        try
-        {
-            FileInputStream forkliftDownInputStream = new FileInputStream(this.getClass().getResource("/images/forklift_down.png").getPath());
-            // this.forkliftDownImage = new Image(forkliftDownInputStream);
+        Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.drawPixel(0, 0);
+        this.drawerTexture = new Texture(pixmap); // remember to dispose of later
+        pixmap.dispose();
 
-            FileInputStream forkliftUpInputStream = new FileInputStream(this.getClass().getResource("/images/forklift_up.png").getPath());
-            // this.forkliftUpImage = new Image(forkliftUpInputStream);
-        }
-        catch (Exception e)
-        {
-            System.out.println("ERROR: INVALID IMAGE");             
-        }
+        this.forkliftDownImage = new Texture(Gdx.files.internal("images/forklift_down.png"));
+        this.forkliftUpImage = new Texture(Gdx.files.internal("images/forklift_up.png"));
+
+        this.setSize(ForkliftSimulator.FrameDimension, ForkliftSimulator.FrameDimension);
     }
 
     @Override
@@ -181,8 +180,8 @@ public class ForkliftSimulator extends SimulatorBase
         {
             FauxbotMotorBase leftDriveMotor = (FauxbotMotorBase)leftDriveActuator;
             FauxbotMotorBase rightDriveMotor = (FauxbotMotorBase)rightDriveActuator;
-            this.leftPower = leftDriveMotor.get();
-            this.rightPower = rightDriveMotor.get();
+            this.leftPower = (float)leftDriveMotor.get();
+            this.rightPower = (float)rightDriveMotor.get();
         }
 
         if (lifterActuator != null && lifterActuator instanceof FauxbotDoubleSolenoid)
@@ -197,20 +196,20 @@ public class ForkliftSimulator extends SimulatorBase
     private void updateOdometry(float delta)
     {
         // check the current distance recorded by the encoders
-        double leftDistance = this.prevLeftDistance + this.leftPower * ForkliftSimulator.FORKLIFT_SPEED * delta;
-        double rightDistance = this.prevRightDistance + this.rightPower * ForkliftSimulator.FORKLIFT_SPEED * delta;
+        float leftDistance = this.prevLeftDistance + this.leftPower * ForkliftSimulator.FORKLIFT_SPEED * delta;
+        float rightDistance = this.prevRightDistance + this.rightPower * ForkliftSimulator.FORKLIFT_SPEED * delta;
 
         // calculate the angle (in radians) based on the total distance traveled
-        double angleR = ForkliftSimulator.STARTING_ANGLE_R + (rightDistance - leftDistance) / ForkliftSimulator.WHEEL_SEPARATION_DISTANCE;
+        float angleR = ForkliftSimulator.STARTING_ANGLE_R + (rightDistance - leftDistance) / ForkliftSimulator.WHEEL_SEPARATION_DISTANCE;
 
         // calculate the average distance traveled
-        double averagePositionChange = ((leftDistance - this.prevLeftDistance) + (rightDistance - this.prevRightDistance)) / 2.0;
+        float averagePositionChange = ((leftDistance - this.prevLeftDistance) + (rightDistance - this.prevRightDistance)) / 2.0f;
 
         // calculate the change since last time, and update our relative position
-        double newX = this.x + averagePositionChange * Math.cos(angleR);
-        double newY = this.y + averagePositionChange * Math.sin(angleR);
+        float newX = this.x + averagePositionChange * (float)Math.cos(angleR);
+        float newY = this.y + averagePositionChange * (float)Math.sin(angleR);
 
-        double newAngle = (angleR * 360.0 / (2.0 * Math.PI)) % 360.0;
+        float newAngle = (angleR * 360.0f / (2.0f * (float)Math.PI)) % 360.0f;
 
         // quick check for collision with walls
         this.x = ForkliftSimulator.clamp(newX, ForkliftSimulator.MAX_WALL_DISTANCE, ForkliftSimulator.MAX_X - ForkliftSimulator.MAX_WALL_DISTANCE);
@@ -222,84 +221,101 @@ public class ForkliftSimulator extends SimulatorBase
         this.prevRightDistance = rightDistance;
     }
 
-    // @Override
-    // public void draw(Canvas canvas)
-    // {
-    //     double canvasHeight = canvas.getHeight();
-    //     double canvasWidth = canvas.getWidth();
+    /**
+     * Draw a frame of animation based on the current state of the simulation.
+     * Remember that (0, 0) is at the bottom left!
+     */
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+        super.draw(batch, parentAlpha);
 
-    //     GraphicsContext gc = canvas.getGraphicsContext2D();
-    //     gc.clearRect(0, 0, canvasWidth, canvasHeight);
+        float frameX = this.getX();
+        float frameY = this.getY();
+        float frameHeight = this.getHeight();
+        float frameWidth = this.getWidth();
 
-    //     gc.setStroke(Color.BLACK);
-    //     gc.strokeRect(0, 0, MAX_X, MAX_Y);
-    //     gc.save();
+        ShapeDrawer drawer = new ShapeDrawer(batch, new TextureRegion(this.drawerTexture, 0, 0, 1, 1));
 
-    //     gc.transform(new Affine(new Rotate(-this.angle, this.x, ForkliftSimulator.MAX_Y - this.y)));
-    //     gc.setFill(Color.RED);
-    //     gc.fillRect(
-    //         this.x - ForkliftSimulator.FORKLIFT_HALF_LENGTH,
-    //         (ForkliftSimulator.MAX_Y - this.y) - ForkliftSimulator.FORKLIFT_HALF_WIDTH,
-    //         ForkliftSimulator.FORKLIFT_LENGTH,
-    //         ForkliftSimulator.FORKLIFT_WIDTH);
-    //     gc.restore();
+        /*
+        drawer.setColor(Color.BLACK);
+        drawer.filledRectangle(frameX, frameY, MAX_X, MAX_Y);
+        gc.strokeRect(0, 0, MAX_X, MAX_Y);
+        gc.save();
 
-    //     gc.setFill(Color.BLUE);
-    //     gc.fillOval(this.x - 1, (ForkliftSimulator.MAX_Y - this.y) - 1, 2, 2);
+        gc.transform(new Affine(new Rotate(-this.angle, this.x, ForkliftSimulator.MAX_Y - this.y)));
+        gc.setFill(Color.RED);
+        gc.fillRect(
+            this.x - ForkliftSimulator.FORKLIFT_HALF_LENGTH,
+            (ForkliftSimulator.MAX_Y - this.y) - ForkliftSimulator.FORKLIFT_HALF_WIDTH,
+            ForkliftSimulator.FORKLIFT_LENGTH,
+            ForkliftSimulator.FORKLIFT_WIDTH);
+        gc.restore();
 
-    //     /*
-    //     double halfHeight = canvasHeight / 2.0;
-    //     double powerIndicatorWidth = canvasWidth / 10.0;
+        gc.setFill(Color.BLUE);
+        gc.fillOval(this.x - 1, (ForkliftSimulator.MAX_Y - this.y) - 1, 2, 2);
+        */
 
-    //     double leftHeight = Math.abs(leftPower * halfHeight);
-    //     double leftTop;
-    //     if (leftPower > 0.0)
-    //     {
-    //         leftTop = halfHeight - leftHeight;
-    //     }
-    //     else
-    //     {
-    //         leftTop = halfHeight;
-    //     }
+        float halfHeight = frameHeight / 2.0f;
+        float powerIndicatorWidth = frameWidth / 10.0f;
 
-    //     gc.setFill(Color.BLUE); 
-    //     gc.fillRect(0, leftTop, powerIndicatorWidth, leftHeight);
+        float leftHeight = Math.abs(leftPower * halfHeight);
+        float leftBottom;
+        if (leftPower > 0.0)
+        {
+            leftBottom = halfHeight;
+        }
+        else
+        {
+            leftBottom = halfHeight - leftHeight;
+        }
 
-    //     double rightHeight = Math.abs(rightPower * halfHeight);
-    //     double rightTop;
-    //     if (rightPower > 0.0)
-    //     {
-    //         rightTop = halfHeight - rightHeight;
-    //     }
-    //     else
-    //     {
-    //         rightTop = halfHeight;
-    //     }
+        drawer.setColor(Color.BLUE); 
+        drawer.filledRectangle(frameX, frameY + leftBottom, powerIndicatorWidth, leftHeight);
 
-    //     gc.setFill(Color.RED); 
-    //     gc.fillRect(powerIndicatorWidth * 2, rightTop, powerIndicatorWidth, rightHeight);
-    //     */
+        float rightHeight = Math.abs(rightPower * halfHeight);
+        float rightBottom;
+        if (rightPower > 0.0)
+        {
+            rightBottom = halfHeight;
+        }
+        else
+        {
+            rightBottom = halfHeight - rightHeight;
+        }
 
-    //     // draw the forklift in its current state
-    //     Image forkliftToDraw = null;
-    //     if (this.forkliftUp)
-    //     {
-    //         forkliftToDraw = this.forkliftUpImage;
-    //     }
-    //     else
-    //     {
-    //         forkliftToDraw = this.forkliftDownImage;
-    //     }
+        drawer.setColor(Color.RED);
+        drawer.filledRectangle(frameX + powerIndicatorWidth * 2.0f, frameY + rightBottom, powerIndicatorWidth, rightHeight);
 
-    //     gc.drawImage(
-    //         forkliftToDraw,
-    //         ForkliftSimulator.MAX_X + 5,
-    //         canvasHeight / 4.0, 
-    //         canvasWidth - (ForkliftSimulator.MAX_X + 5),
-    //         canvasHeight / 2.0);
-    // }
+        // draw the forklift in its current state
+        Texture forkliftToDraw = null;
+        if (this.forkliftUp)
+        {
+            forkliftToDraw = this.forkliftUpImage;
+        }
+        else
+        {
+            forkliftToDraw = this.forkliftDownImage;
+        }
+        
+        batch.draw(
+            forkliftToDraw,
+            frameX + ForkliftSimulator.MAX_X + 5.0f,
+            frameY + frameHeight / 4.0f,
+            frameWidth - (ForkliftSimulator.MAX_X + 5),
+            frameHeight / 2.0f);
+    }
 
-    private static double clamp(double value, double min, double max)
+    @Override
+    public void dispose()
+    {
+        super.dispose();
+        this.drawerTexture.dispose();
+        this.forkliftDownImage.dispose();
+        this.forkliftUpImage.dispose();
+    }
+
+    private static float clamp(float value, float min, float max)
     {
         if (value < min)
         {
