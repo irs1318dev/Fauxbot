083660faca5f91c8f6d724861d324d665eaa169a
Emily N <emilycnie@gmail.com>
2024-12-12 15:52:08 -0800

    Update forkLiftMechanism.java

--- core_robot/src/main/java/frc/robot/Mechanisms/forkLiftMechanism.java
+++ core_robot/src/main/java/frc/robot/Mechanisms/forkLiftMechanism.java
@@ -1,9 +1,30 @@
 package frc.robot.Mechanisms;
 
 import frc.lib.mechanisms.IMechanism;
+import frc.robot.common.*;
+import java.security.Provider;
+
+import com.google.inject.Inject;
+import com.google.inject.singleton;
+
+import frc.lib.driver.IDriver;
+import frc.lib.mechanisms.GarageDoor;
+import frc.lib.mechanisms.GarageDoor.GarageDoorState;
+import frc.lib.robotprovider.IDigitalInput;
+import frc.lib.robotprovider.IMotor;
+import frc.lib.robotprovider.IRobotProvider;
+import frc.robot.ElectronicsConstants;
+import frc.robot.EletronicsConstants;
+import frc.robot.LoggingKey;
+import frc.robot.TuningConstants;
+import frc.robot.common.IMechanism;
+import frc.robot.common.LoggingManager;
+import frc.robot.common.robotprovider.*;
+import frc.robot.driver.*;
+import frc.robot.driver.common.Idriver;
 
 @Singleton
-public class ForkLiftMechanism implements IMechanism
+public class forkLiftMechanism implements IMechanism
 {
     // Lifter
     private final IDoubleSolenoid lifter;
@@ -14,7 +35,7 @@ public class ForkLiftMechanism implements IMechanism
     private final IDriver driver;
 
     @Inject
-    public Forkliftmechanism(IRobotProvider provider, IDriver driver)
+    public forkLiftMechanism(IRobotProvider provider, IDriver driver)
     {
         this.driver = driver;
         this.leftMotor = provider.getTalon(ElectronicsConstants.FORLIFT_LEFT_MOTOR_CHANNEL);
@@ -42,7 +63,7 @@ public class ForkLiftMechanism implements IMechanism
         {
             this.lifter.set(DoubleSolenoidValue.Forward);
         }
-        else (this.driver.getAnalogOperation(AnalogOperation.TurnRight))
+        else if (this.driver.getAnalogOperation(AnalogOperation.TurnRight))
         {
             this.lifter.set(DoubleSolenoidValue.Forward);
         }
@@ -52,10 +73,10 @@ public class ForkLiftMechanism implements IMechanism
     @Override
     public void stop()
     {
-        this.leftMotor.set(power:0.0);
+        this.leftMotor.set(0.0);
         this.lifter.set(DoubleSolenoidValue.Off);
        
-        this.rightMotor.set(power:1.0);
+        this.rightMotor.set(1.0);
         this.lifter.set(DoubleSolenoidValue.Off);
     }
 }
